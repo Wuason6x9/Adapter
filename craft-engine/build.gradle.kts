3233719bd923bccee7aa6107f52363d2121dc642
Masaki Dev <chris.rojas.r2007@gmail.com>
2025-07-26 18:36:42 -0600

    Updated to CraftEngine 0.0.60

--- craft-engine/build.gradle.kts
+++ craft-engine/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT") //Vanilla
-    compileOnly("net.momirealms:craft-engine-core:0.0.59")
-    compileOnly("net.momirealms:craft-engine-bukkit:0.0.59")
+    compileOnly("net.momirealms:craft-engine-core:0.0.60")
+    compileOnly("net.momirealms:craft-engine-bukkit:0.0.60")
     compileOnly(project(":common"))
 }
 
