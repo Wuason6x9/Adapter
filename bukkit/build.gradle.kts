2fece96e09445c167d662c35391ec24866bc4082
wuason <daniperezmartinez48@gmail.com>
2025-07-28 21:00:08 +0200

    Update dependencies and enhance Gradle scripts for robustness
    Upgraded Oraxen dependency and Gradle distribution version. Improved Gradle scripts with better error handling, clarified comments, and added SPDX-License-Identifier. Ensured stricter validation for URLs and system commands.

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
 
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT") //Vanilla
     compileOnly("dev.lone:api-itemsadder:4.0.2-beta-release-11") //ItemsAdder
-    compileOnly("com.github.oraxen:oraxen:1.159.0") //Oraxen 1
+    compileOnly("io.th0rgal:oraxen:1.163.0") //Oraxen 1
     compileOnly(fileTree("libs")) // ExecutableItems and ExecutableBlocks
     implementation(fileTree("libs-impl")) // StorageMechanic and CustomItems
     compileOnly("net.Indyuce:MMOItems-API:6.9.5-SNAPSHOT") //MMOItems
